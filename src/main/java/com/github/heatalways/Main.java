502fd23a940cec2e5f766f35ac50fc7144b81311
HeatAlways <kazyxanovr1@gmail.com>
2018-11-17 20:00:43 +0300

    new Exception: NullPointerException

--- src/main/java/com/github/heatalways/Main.java
+++ src/main/java/com/github/heatalways/Main.java
@@ -1,10 +1,13 @@
 package com.github.heatalways;
 
 
+import com.github.heatalways.vkAuth.VkAuth;
+
 import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+        VkApi vkApi = new VkApi(new VkAuth().setAccessToken("563b41b288f60cb0b921aaec532aee78ff620375d1063a62fce239b06356df3aa45b9bc4c7ad54018ff44"));
 
     }
 }
--- src/main/java/com/github/heatalways/utils/ArrayToString.java
+++ src/main/java/com/github/heatalways/utils/ArrayToString.java
@@ -20,6 +20,8 @@ public class ArrayToString {
             return str.toString();
         } catch (ArrayIndexOutOfBoundsException e) {
             return "";
+        } catch (NullPointerException e) {
+            return "";
         }
     }
 }
